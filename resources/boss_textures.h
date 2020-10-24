a876633f77df6a918dab3ee1a0949d2988c959f4
LuanAccioly <acciolyluan@gmail.com>
2020-10-24 16:51:36 -0300

    feat: load boss textures

---
+++ resources/boss_textures.h
@@ -0,0 +1,54 @@
+#ifndef BOSS_TEXTURES_H
+#define BOSS_TEXTURES_H
+
+#include "raylib.h"
+
+static Texture2D bossIdle[12];
+static Texture2D bossDying[15];
+static Texture2D bossCasting[18];
+
+
+void LoadBossTextures(){
+    
+    for ( int x = 0; x < 12; x++ ) {
+        bossIdle[x] = LoadTexture(
+            x > 9 ? 
+            FormatText("resources/textures/boss/Running/Wraith_01_Idle_0%i.png", x) :
+            FormatText("resources/textures/boss/Running/Wraith_01_Idle_00%i.png", x)
+        );
+    }
+
+    for ( int x = 0; x < 18; x++ ) {
+        bossCasting[x] = LoadTexture(
+            x > 9 ? 
+            FormatText("resources/textures/boss/Running/Wraith_01_Casting Speels_0%i.png", x) :
+            FormatText("resources/textures/boss/Running/Wraith_01_Casting Speels_00%i.png", x)
+        );
+    }
+
+    for ( int x = 0; x < 15; x++ ) {
+        bossDying[x] = LoadTexture(
+            x > 9 ? 
+            FormatText("resources/textures/boss/Running/Wraith_01_Dying_0%i.png", x) :
+            FormatText("resources/textures/boss/Running/Wraith_01_Dying_00%i.png", x)
+        );
+    }
+}
+
+void UnloadCharacterTextures() {
+
+    for ( int x = 0; x < 12; x++ ) {
+        UnloadTexture(bossIdle[x]);
+    }
+
+    for ( int x = 0; x < 18; x++ ) {
+        UnloadTexture(bossCasting[x]);
+    }
+
+    for ( int x = 0; x < 15; x++ ) {
+        UnloadTexture(bossDying[x]);
+    }
+
+}
+
+#endif
